5c9746ecf2b7b18909fd576844b8c844779b84de
Jay Lawrence <jay@patientway.com>
2015-05-06 08:10:07 -0400

    generalize and add item subfield splitting (brute force for now)

--- lib/segments/pid.rb
+++ lib/segments/pid.rb
@@ -3,16 +3,20 @@ require 'ruby-hl7'
 class HL7::Message::Segment::PID < HL7::Message::Segment
   weight 1
   has_children [:NK1,:NTE,:PV1,:PV2]
-  add_field :set_id, :idx => 0
-  add_field :patient_id, :idx => 1
-  add_field :patient_id_list, :idx => 2
-  add_field :alt_patient_id, :idx => 3
-  add_field :patient_name, :idx => 4
-  add_field :mother_maiden_name, :idx => 5
-  add_field :patient_dob, :idx => 6 do |value|
-    convert_to_ts(value)
+  add_field :set_id
+  add_field :patient_id do |value|
+    value.split('^')
   end
-  add_field :admin_sex, :idx => 7 do |sex|
+  add_field :patient_id_list
+  add_field :alt_patient_id
+  add_field :patient_name do |value|
+    value.split('^')
+  end
+  add_field :mother_maiden_name
+  add_field :patient_dob do |value|
+    convert_to_datetime(value)
+  end
+  add_field :admin_sex do |sex|
     unless /^[FMOUANC]$/.match(sex) || sex == nil || sex == ""
       raise HL7::InvalidDataError.new( "bad administrative sex value (not F|M|O|U|A|N|C)" )
     end
@@ -21,7 +25,9 @@ class HL7::Message::Segment::PID < HL7::Message::Segment
   end
   add_field :patient_alias
   add_field :race
-  add_field :address
+  add_field :address do |value|
+    value.split('^')
+  end
   add_field :country_code
   add_field :phone_home
   add_field :phone_business
